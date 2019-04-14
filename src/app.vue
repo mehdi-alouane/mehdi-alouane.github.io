2e15d16e7076eb7c5f7caa8a94b05bab8a8ad55c
mehdi-alouane <pro4medo@gmail.com>
2019-04-14 14:27:23 +0100

    add bulma & font-awesome to app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Hello world from vue!</h1>
+  <card
+      init="Hello World 👋️"
+      name="i'm Mehdi Alouane"
+      description="A self taught fullstack developer"
+    />
 </template>
+
+<script>
+import Card from './components/card.vue'
+
+export default {
+  components: {
+    Card
+  }
+}
+</script>
+
+<style lang="sass">
+@import url('../node_modules/bulma/css/bulma.css')
+@import url('../node_modules/font-awesome/css/font-awesome.css')
+</style>
