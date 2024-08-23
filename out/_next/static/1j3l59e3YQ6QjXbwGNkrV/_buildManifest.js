038660353508e417d3a743227b59249dfd91a86a
mehdi-alouane <meehdialouane@gmail.com>
2024-08-23 16:00:56 +0100

    Add out dir

---
+++ out/_next/static/1j3l59e3YQ6QjXbwGNkrV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/721-c2a5084ee5a62516.js","static/chunks/pages/index-45bff684cc879598.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
